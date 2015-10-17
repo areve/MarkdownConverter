64a41f21893948d82ffd74af7b965c38b9184cf7
andrew <none@none>
2015-10-17 23:10:42 +0100

    standalone build

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
-
+@echo off
 cd %~dp0
 
-..\..\node2exe\examples\Node2exeTool\Node2exeTool.exe MarkdownConverter.js MarkdownConverter.n2e marked.js html-entities.js windows-1250.js -filedescription "Tool to convert markdown files to HTML" -icon favicon.ico -legalcopyright "AcServer Copyright Andrew Challen, MIT license; Node Copyright Joyent, Inc. and other Node contributors. MIT license."
+if exist ..\node2exe\node2exe_tool.js (
+	node ..\node2exe\node2exe_tool.js MarkdownConverter.exe MarkdownConverter.js marked.js html-entities.js windows-1250.js -filedescription "Tool to convert markdown files to HTML" -icon favicon.ico -copyright "MarkdownConverter Copyright Andrew Challen, MIT license; Node Copyright Joyent, Inc. and other Node contributors. MIT license."
+)
+rem ..\node2exe\examples\Node2exeTool\Node2exeTool.exe MarkdownConverter.js MarkdownConverter.n2e marked.js html-entities.js windows-1250.js -filedescription "Tool to convert markdown files to HTML" -icon favicon.ico -legalcopyright "AcServer Copyright Andrew Challen, MIT license; Node Copyright Joyent, Inc. and other Node contributors. MIT license."
 
-if exist ..\..\portable_apps copy /y MarkdownConverter.exe ..\..\portable_apps
+if exist ..\portable_apps copy /y MarkdownConverter.exe ..\portable_apps
